6c5a66070782dfc5977868915f9c2b25ca83bf56
TheWay <s19005313@yahoo.com.tw>
2020-06-02 10:14:16 +0800

    commit master

--- hello.py
+++ hello.py
@@ -3,3 +3,4 @@ print('hello python & git second++')
 switch master and push 20200529 16:36
 switch from cat to origin
 modified dev branch 20200602 09:20
+add something in master branch 20200602 10:11
