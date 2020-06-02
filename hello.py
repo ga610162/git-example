107b62beedec1005c57a0318cbb1182bebe4e435
TheWay <s19005313@yahoo.com.tw>
2020-06-02 10:17:49 +0800

    checkout 'cat' and add something

--- hello.py
+++ hello.py
@@ -4,3 +4,5 @@ switch master and push 20200529 16:36
 switch from cat to origin
 modified dev branch 20200602 09:20
 add something in master branch 20200602 10:11
+
+checkout branch 'cat' and add something 20200602 10:16
